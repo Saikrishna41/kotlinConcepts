fa011e84a860b91a5be01a6c5a8e0272a4657242
Saikrishna41 <saai.krrishna@gmail.com>
2022-12-14 13:26:12 +0800

    inheritancedemo

--- src/main/java/InheritanceDemo.kt
+++ src/main/java/InheritanceDemo.kt
@@ -1,6 +1,25 @@
-class Base(name : String) {
+open class BaseClass(name: String) {
 
     init {
-        println()
+        println("Base class initialized")
     }
+
+    open val size: Int = name.length.also { println("Initializing size in base class $it") }
+}
+
+class DerivedClass(name: String, val lastName: String) : BaseClass(name.replaceFirstChar { it.uppercase() }.also {
+
+    println("Argument for baseclass $it")
+}) {
+    init {
+        println("Derived class initialized")
+    }
+    override val size: Int = (super.size + lastName.length).also { println("Initializing size in derived class $it") }
+}
+
+fun main() {
+
+    println("Constructing derived class (\"hello\" , \"world\")")
+    DerivedClass("hello","world")
+
 }
